490f86b6d80a5a67ef4e4ec8bb46c152d7c03985
Goliaten <ognikat4@gmail.com>
2026-02-11 14:02:08 +0100

    alter printout wrapper to use logger

--- source/helpers/printout_helper.py
+++ source/helpers/printout_helper.py
@@ -1,9 +1,12 @@
+from source.Logger import Logger
+
+
 def in_out_wrapper(fnc):
 
     def inner(*args, **kwargs):
-        print(f"Entering {fnc.__name__}")
+        Logger().get().debug(f"Entering {fnc.__name__}")
         out = fnc(*args, **kwargs)
-        print(f"Exiting {fnc.__name__}")
+        Logger().get().debug(f"Exiting {fnc.__name__}")
         return out
 
     return inner
